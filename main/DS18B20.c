9604eb8fe121a432deda753e50109865ab3e6733
ChrisThompson <christopher123@me.com>
2021-09-13 17:59:24 -0500

    Fixed write to DS18B20. Made changes to read, need to verify bytes.

--- main/DS18B20.c
+++ main/DS18B20.c
@@ -15,6 +15,7 @@
 #include "esp_err.h"
 
 static const char *TAG = "DS18B20 Driver";
+int count = 0;
 bool timeout;
 bool present;
 
@@ -24,6 +25,7 @@ void hw_timer_callback(void *arg){
 
 void gpio_isr_handler(void *arg){
     present = true;
+    count++;
 }
 
 //This method performs a reset, and presence check on the DS18B20. 
@@ -39,10 +41,13 @@ bool DS18B20_InitializeBus(gpio_num_t gpio_num){
     
     //Send reset pulse for a minimum of 480us
     ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_OUTPUT));
-    vTaskDelay(pdMS_TO_TICKS(1));
+    ESP_ERROR_CHECK(hw_timer_init(hw_timer_callback, NULL));
+    ESP_ERROR_CHECK(gpio_set_level(gpio_num, 0));       
+    ESP_ERROR_CHECK(hw_timer_alarm_us(500, false));
+    while(!timeout){}; //busy wait
+    timeout = false;
     
     //Configure falling-edge interrupt to catch 'presence' signal from DS18B20
-    ESP_ERROR_CHECK(gpio_set_level(gpio_num, 0));
     ESP_ERROR_CHECK(gpio_set_intr_type(gpio_num, GPIO_INTR_NEGEDGE));
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
     ESP_ERROR_CHECK(gpio_isr_handler_add(gpio_num, gpio_isr_handler, (void *)gpio_num));
@@ -50,9 +55,7 @@ bool DS18B20_InitializeBus(gpio_num_t gpio_num){
     //Set the bus back high, wait 60us max for presence signal, or timeout
     ESP_ERROR_CHECK(gpio_set_level(gpio_num, 1));
     ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_INPUT));
-    ESP_ERROR_CHECK(hw_timer_init(hw_timer_callback, NULL));
-    ESP_ERROR_CHECK(hw_timer_alarm_us(60, false));
-    
+    ESP_ERROR_CHECK(hw_timer_alarm_us(100, false));    
     
     while(!timeout && !present){
         //Wait until DS18B20 is present, or we timeout. Whichever comes first. 
@@ -62,6 +65,7 @@ bool DS18B20_InitializeBus(gpio_num_t gpio_num){
     ESP_ERROR_CHECK(hw_timer_deinit());
     ESP_ERROR_CHECK(gpio_isr_handler_remove(gpio_num));
     gpio_uninstall_isr_service();
+    ESP_ERROR_CHECK(gpio_set_intr_type(gpio_num, GPIO_INTR_DISABLE));
     
     return present; 
 }
@@ -69,24 +73,30 @@ bool DS18B20_InitializeBus(gpio_num_t gpio_num){
 void DS18B20_WriteBit(gpio_num_t gpio_num, bool bit){
     timeout = false;
     //int writeValue = bit ? 0 : 1;
-    ESP_LOGI(TAG, "Writing bit %d", bit);
+    //ESP_LOGI(TAG, "Writing bit %d", bit);
     ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_OUTPUT));
     ESP_ERROR_CHECK(gpio_set_level(gpio_num, 0));
     ESP_ERROR_CHECK(hw_timer_init(hw_timer_callback, NULL));
-    ESP_ERROR_CHECK(hw_timer_alarm_us(11, false));
-    while(!timeout){}; //busy wait
-    timeout = false;
+    //ESP_ERROR_CHECK(hw_timer_alarm_us(11, false));
+    //while(!timeout){}; //busy wait
+    //timeout = false;
     ESP_ERROR_CHECK(gpio_set_level(gpio_num, bit));
+    if(bit){
+        ESP_ERROR_CHECK(gpio_set_direction(gpio_num, 1));
+    }
     ESP_ERROR_CHECK(hw_timer_alarm_us(60, false));
     while(!timeout){}; //busy wait
-    
+    timeout = false;
     //Release Bus
+    ESP_ERROR_CHECK(gpio_set_direction(gpio_num, 1));
     ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_INPUT));
+    ESP_ERROR_CHECK(hw_timer_alarm_us(11, false));
+    while(!timeout){};
     ESP_ERROR_CHECK(hw_timer_deinit());
 }
 
 void DS18B20_WriteByte(gpio_num_t gpio_num, uint8_t byte){
-    ESP_LOGI(TAG, "Writing byte 0x%x", byte);
+    //ESP_LOGI(TAG, "Writing byte 0x%x", byte);
     for(int i = 0; i < 8; i++){
         DS18B20_WriteBit(gpio_num, (byte & 1<<i) > 0);
     }
@@ -94,27 +104,26 @@ void DS18B20_WriteByte(gpio_num_t gpio_num, uint8_t byte){
 
 uint8_t DS18B20_ReadBit(gpio_num_t gpio_num){
     timeout = false;
-    
+    present = false;
     ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_OUTPUT));
     ESP_ERROR_CHECK(gpio_set_level(gpio_num, 0));
     ESP_ERROR_CHECK(hw_timer_init(hw_timer_callback, NULL));
+    //ESP_ERROR_CHECK(hw_timer_alarm_us(11, false));
+    //while(!timeout){}; //busy wait
+    ESP_ERROR_CHECK(gpio_set_intr_type(gpio_num, GPIO_INTR_NEGEDGE));
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+    ESP_ERROR_CHECK(gpio_isr_handler_add(gpio_num, gpio_isr_handler, (void *)gpio_num));
+    ESP_ERROR_CHECK(gpio_set_level(gpio_num, 1));
+    ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_INPUT));
     ESP_ERROR_CHECK(hw_timer_alarm_us(11, false));
     while(!timeout){}; //busy wait
     timeout = false;
-    present = false;
-    ESP_ERROR_CHECK(gpio_set_intr_type(gpio_num, GPIO_INTR_NEGEDGE));
-    ESP_ERROR_CHECK(gpio_install_isr_service(0));
-    ESP_LOGI(TAG, "Interrupt fired %d", present);
-    ESP_ERROR_CHECK(gpio_isr_handler_add(gpio_num, gpio_isr_handler, (void *)gpio_num));
-    ESP_LOGI(TAG, "Interrupt fired %d", present);
-    ESP_ERROR_CHECK(gpio_set_level(gpio_num, 1));
-    ESP_ERROR_CHECK(gpio_set_direction(gpio_num, GPIO_MODE_INPUT));
-    ESP_ERROR_CHECK(hw_timer_alarm_us(60, false));
-    while(!timeout){}; //busy wait
-    ESP_ERROR_CHECK(hw_timer_deinit());
     ESP_ERROR_CHECK(gpio_isr_handler_remove(gpio_num));
     gpio_uninstall_isr_service();
-    ESP_LOGI(TAG, "Reading bit %d", !present);
+    ESP_ERROR_CHECK(gpio_set_intr_type(gpio_num, GPIO_INTR_DISABLE));
+    ESP_ERROR_CHECK(hw_timer_alarm_us(50, false));
+    while(!timeout){};
+    ESP_ERROR_CHECK(hw_timer_deinit());
     return (uint8_t)(!present);
 
 }
--- main/Temperature.c
+++ main/Temperature.c
@@ -20,23 +20,29 @@ static const char *TAG = "Temperature Task";
 void temperature_task(void *pvParameters){
 
     //Pin "D2" is GPIO_NUM_4
-    if(DS18B20_InitializeBus(GPIO_NUM_4)){
-        ESP_LOGI(TAG, "DS18B20 Initialized Successfully");
-        DS18B20_WriteByte(GPIO_NUM_4, 0xCC);
-        DS18B20_WriteByte(GPIO_NUM_4, 0xBE);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-        DS18B20_ReadByte(GPIO_NUM_4);
-    }else{
-        ESP_LOGE(TAG, "Sensor failed to intialize");
+    while(1){
+	    if(DS18B20_InitializeBus(GPIO_NUM_4)){
+		ESP_LOGI(TAG, "DS18B20 Initialized Successfully");
+		
+		DS18B20_WriteByte(GPIO_NUM_4, 0xCC); //LSB First - 00110011
+		DS18B20_WriteByte(GPIO_NUM_4, 0xBE); //LSB First - 01111101
+		DS18B20_ReadByte(GPIO_NUM_4);
+		
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		DS18B20_ReadByte(GPIO_NUM_4);
+		
+		
+	    }else{
+		ESP_LOGE(TAG, "Sensor failed to intialize");
+	    }
+	    vTaskDelay(pdMS_TO_TICKS(1000));
     }
-    
 
     while(1){
         vTaskDelay(pdMS_TO_TICKS(100));
